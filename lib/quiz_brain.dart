95341d856b23f1c2922186713f8d9cb22cfbbd32
Procoder16 <smsoumik1608@gmail.com>
2021-04-18 01:44:49 +0530

    changed the questions

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -4,31 +4,28 @@ class QuizBrain {
   int _questionNumber = 0;
 
   List<Question> _questionBank = [
-    Question('Some cats are actually allergic to humans', true),
+    Question(
+        'The first man to climb the Mt. Everest was Neil Armstrong.', false),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
-    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
-    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question('SpaceX is a company owned by Jeff Bezos.', false),
+    Question(
+        'The first player to hit a double century in One Day Cricket was Sachin Tendulkar.',
+        true),
     Question(
         'No piece of square dry paper can be folded in half more than 7 times.',
         false),
-    Question(
-        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
-        true),
+    Question('The Fax Machine was patented in 1843.', true),
     Question(
         'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
         false),
     Question(
-        'The total surface area of two human lungs is approximately 70 square metres.',
-        true),
+        'The total surface area of two human lungs is approximately 100 square metres.',
+        false),
     Question('Google was originally called \"Backrub\".', true),
-    Question(
-        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
-        true),
-    Question(
-        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
-        true),
+    Question('Darker the coffee roast, more the caffeine it contains.', false),
+    Question('The Statue of Liberty was a gift from France.', true),
   ];
 
   void nextQuestion() {
@@ -53,7 +50,6 @@ class QuizBrain {
 
       print('Now returning true');
       return true;
-
     } else {
       return false;
     }
